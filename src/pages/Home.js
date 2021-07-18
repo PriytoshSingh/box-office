a3ad089357e0176e67f207d86f394ed0321602bd
Priytosh Kumar Singh <priytosh1234@gmail.com>
2021-07-18 15:25:53 +0530

    fixed typo error

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
 
   const onSearch = () => {
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r =>
-      r.json().then(result => {
-        console.log(result);
-      })
-    );
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+    });
   };
 
   const onInputChange = ev => {
@@ -22,6 +22,23 @@ const Home = () => {
     }
   };
 
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div> No results </div>;
+    }
+
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+    return null;
+  };
+
   return (
     <MainPageLayout>
       <input
@@ -30,9 +47,11 @@ const Home = () => {
         onKeyDown={onKeyDown}
         value={input}
       />
+
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
