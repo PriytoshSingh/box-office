334b76099f0b001b64726c04cd6d26454df61114
Priytosh Kumar Singh <priytosh1234@gmail.com>
2021-07-19 16:22:46 +0530

    styled show page

---
+++ src/components/show/Details.styled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const DetailsWrapper = styled.div`
+  p {
+    margin: 5px 0;
+
+    span {
+      font-weight: bold;
+    }
+  }
+`;
+
+// Seasons.styled.js
+export const SeasonsWrapper = styled.div`
+  p {
+    margin: 5px 0;
+  }
+
+  span {
+    font-weight: 700;
+  }
+`;
